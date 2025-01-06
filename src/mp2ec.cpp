2ae0e465a88979016c79dc285a81d94510623c72
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-06 17:56:17 -0500

    Get input working

--- src/mp2ec.cpp
+++ src/mp2ec.cpp
@@ -1,6 +1,6 @@
-#include "vector";
-#include <iostream>;
-#include <string>;
+#include "vector"
+#include <iostream>
+#include <string>
 using namespace std;
 
 enum CellStatus {
@@ -9,44 +9,50 @@ enum CellStatus {
   Uninfected,
 };
 
-CellStatus transpileStatus(char);
-char transpileStatus(CellStatus);
+CellStatus translateStatus(char);
+char translateStatus(CellStatus);
 
 void printMatrix(vector<vector<CellStatus>>);
+void simulatePlague(vector<vector<CellStatus>>);
 
 int main() {
+  // The size of the square grid
   int S;
-  cin >> S;
 
+  // The number of hours the simulation runs for
   int T;
-  cin >> T;
 
-  vector<vector<CellStatus>> matrix(S);
+  cin >> S >> T;
 
-  for (int i; i < S; i++) {
-    string input;
+  vector<vector<CellStatus>> matrix(S, vector<CellStatus>(S, Uninfected));
+
+  char input;
+
+  int counter = 0;
+
+  while (counter < S * S) {
     cin >> input;
 
-    for (int j; j < S; j++) {
-      matrix.at(i).at(j) = transpileStatus(input[j]);
-    }
+    matrix.at((int)(counter / S)).at(counter % S) = translateStatus(input);
+
+    counter++;
   }
 
   printMatrix(matrix);
   return 0;
 }
 
-CellStatus transpileStatus(char c) {
-  if (c == 'P')
+CellStatus translateStatus(char c) {
+  if (c == 'P') {
     return Infected;
-  else if (c == 'B') {
+  } else if (c == 'B') {
     return Building;
   } else {
     return Uninfected;
   }
 }
 
-char transpileStatus(CellStatus c) {
+char translateStatus(CellStatus c) {
   if (c == Infected) {
     return 'P';
   } else if (c == Building) {
@@ -58,9 +64,12 @@ char transpileStatus(CellStatus c) {
 
 void printMatrix(vector<vector<CellStatus>> v) {
   for (int i = 0; i < v.size(); i++) {
-    for (int j = 0; j < v.size(); j++) {
-      cout << transpileStatus(v.at(i).at(j)) << " ";
+    for (int j = 0; j < v[i].size(); j++) {
+      cout << translateStatus(v.at(i).at(j)) << " ";
     }
     cout << endl;
   }
+  cout << endl;
 }
+
+void simulatePlague(vector<vector<CellStatus>> v) {}
